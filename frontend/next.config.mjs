eb91e034cf4a818dac5bc93980cb51ea734d0c0c
AvanshChauhan <avanshsingh2017@gmail.com>
2026-05-28 21:32:15 +0530

    Prepare app for deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  outputFileTracingRoot: import.meta.dirname,
+  turbopack: {
+    root: import.meta.dirname,
+  },
 };
 
 export default nextConfig;
